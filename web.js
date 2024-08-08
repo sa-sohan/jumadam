bc11b8730754f64f7c9d0ae08ee8b582b8b75333
Jumadam <jylee@sasohan.net>
2024-08-08 18:09:10 +0900

    Updated web.js to serve static files from public and uploads directories

--- web.js
+++ web.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
+// 모든 라우트를 index.html로 리디렉션
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const port = process.env.PORT || 8001;
